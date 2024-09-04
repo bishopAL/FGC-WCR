d9a132231ac5b1b30f855162c7251ebedc9d53ce
zhang weilong <1791579486@qq.com>
2024-09-04 21:50:48 +0800

    Add files via upload

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/SVnPUMPnIMU/i2c.c" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/SVnPUMPnIMU/i2c.c.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/SVnPUMPnIMU/wit_c_sdk.c" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/SVnPUMPnIMU/wit_c_sdk.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/dynamixel"
+  "../include/INA219"
+  "../include/SVnPUMPnIMU"
+  "../include/MotionControl"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/INA219/GestionI2C.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/INA219/GestionI2C.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/INA219/GestionINA219.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/INA219/GestionINA219.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/INA219/GestionPin.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/INA219/GestionPin.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/INA219/GestionPinGPIO.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/INA219/GestionPinGPIO.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/MotionControl/controlhelper.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/MotionControl/controlhelper.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/MotionControl/gebot.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/MotionControl/gebot.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/MotionControl/leg.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/MotionControl/leg.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/MotionControl/robotcontrol.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/MotionControl/robotcontrol.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/MotionControl/udpsocket.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/MotionControl/udpsocket.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/SVnPUMPnIMU/ADS1x15.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/SVnPUMPnIMU/ADS1x15.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/SVnPUMPnIMU/api.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/SVnPUMPnIMU/api.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/dynamixel/dynamixel.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/dynamixel/dynamixel.cpp.o"
+  "/home/pi/Desktop/FGC-WCR-fnnInferBoost/src/main.cpp" "/home/pi/Desktop/FGC-WCR-fnnInferBoost/build/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/dynamixel"
+  "../include/INA219"
+  "../include/SVnPUMPnIMU"
+  "../include/MotionControl"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/test.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.dir/src/INA219/GestionI2C.cpp.o"
+  "CMakeFiles/test.dir/src/INA219/GestionINA219.cpp.o"
+  "CMakeFiles/test.dir/src/INA219/GestionPin.cpp.o"
+  "CMakeFiles/test.dir/src/INA219/GestionPinGPIO.cpp.o"
+  "CMakeFiles/test.dir/src/MotionControl/controlhelper.cpp.o"
+  "CMakeFiles/test.dir/src/MotionControl/gebot.cpp.o"
+  "CMakeFiles/test.dir/src/MotionControl/leg.cpp.o"
+  "CMakeFiles/test.dir/src/MotionControl/robotcontrol.cpp.o"
+  "CMakeFiles/test.dir/src/MotionControl/udpsocket.cpp.o"
+  "CMakeFiles/test.dir/src/SVnPUMPnIMU/ADS1x15.cpp.o"
+  "CMakeFiles/test.dir/src/SVnPUMPnIMU/api.cpp.o"
+  "CMakeFiles/test.dir/src/SVnPUMPnIMU/i2c.c.o"
+  "CMakeFiles/test.dir/src/SVnPUMPnIMU/wit_c_sdk.c.o"
+  "CMakeFiles/test.dir/src/dynamixel/dynamixel.cpp.o"
+  "CMakeFiles/test.dir/src/test.cpp.o"
+  "test"
+  "test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
